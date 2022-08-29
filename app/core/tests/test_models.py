1ca2b2b33f50f81a5c714fbec058c72140afdf36
Hedryan Rojas <jonhdoe@example.com>
2022-08-29 08:17:33 -0600

    Linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,7 @@ Test for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTest(TestCase):
     """Test models."""
 
@@ -14,8 +15,8 @@ class ModelTest(TestCase):
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
